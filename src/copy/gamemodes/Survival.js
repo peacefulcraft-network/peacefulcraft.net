c3819ca3bc7fa9456fc4408b5855eead5991f6a2
Jumbie247 <65747242+Jumbie247@users.noreply.github.com>
2020-05-22 03:18:31 +0100

    Update Survival.js

--- src/copy/gamemodes/Survival.js
+++ src/copy/gamemodes/Survival.js
@@ -16,8 +16,8 @@ export default {
   },
   
   infoBlurb: `Player versus environment survival is the oldest gamemode on the network. PvE is designed to facilitate a relaxed 
-  and laidback playstyle. Player's have access to unlimited homes, keep inventory on death, a player-run mall with
-  shops and trading, random teleports, night vision, daytime toggle, personal time, custon nicknames, and Towny 
+  and laidback playstyle. Players have access to unlimited homes, keep inventory on death, a player-run mall with
+  shops and trading, random teleports, night vision, daytime toggle, personal time, custom nicknames, and Towny 
   for protecting their builds. Inventories between PvE and Peaceful are shared so anyone wishing to move between 
   worlds can do so seamlessly. The PvE world was last reset in February of 2019 and the world border is expanded 
   regularly with Minecraft updates. RTP ranges allow players to target specific game versions to teleport into so 
@@ -26,29 +26,29 @@ export default {
   connectInstructions: {
     'Hub': [
       {
-        instruction: 'Connect to our network hub server with the following ip.',
+        instruction: 'Connect to our network hub server with the following ip:',
         code: 'peacefulcraft.net',
       },
       {
-        instruction: 'Use the following command to join the PvE Server.',
+        instruction: 'Use the following command to join the PvE Server:',
         code: '/server pve',
       },
       {
-        instruction: 'We recommend using the RTP command to get out of spawn and begin your adventure.',
+        instruction: 'We recommend using the RTP command to get out of spawn and begin your adventure:',
         code: '/rtp',
       },
     ],
     'Direct Connect': [
       {
-        instruction: 'Direct connect to PvE with the follow ip.',
+        instruction: 'Direct connect to PvE with the follow ip:',
         code: 'pve.peacefulcraft.net',
       },
       {
-        instruction: 'We recommend using the RTP command to get out of spawn and begin your adventure.',
+        instruction: 'We recommend using the RTP command to get out of spawn and begin your adventure:',
         code: '/rtp',
       },
       {
-        instruction: 'Players can still switch between servers when using the direct connect domains. The following command will take you to our hub server.',
+        instruction: 'Players can still switch between servers when using the direct connect domains. The following command will take you to our hub server:',
         code: '/server lobby',
       },
     ],
