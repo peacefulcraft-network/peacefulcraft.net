38c3aee0d33d1bda9fae5f005faf4f8f7e4f2814
Jumbie247 <65747242+Jumbie247@users.noreply.github.com>
2020-05-22 03:20:12 +0100

    Update Peaceful.js

--- src/copy/gamemodes/Peaceful.js
+++ src/copy/gamemodes/Peaceful.js
@@ -11,9 +11,9 @@ export default {
     'The Mall - First Anex': MallAnex,
   },
   
-  infoBlurb: `True to it's name, the PeacefulCraft Network has a peaceful survival world. The
+  infoBlurb: `True to its name, the PeacefulCraft Network has a peaceful survival world. The
     peaceful world has all the same amenities as the normal survival world; unlimited homes,
-    teleporting, keep inventory, etc, just without the mobs. Player's can move between the normal
+    teleporting, keep inventory, etc, just without the mobs. Players can move between the normal
     survival world and the peaceful world by warping or teleporting. PeacefulCraft's peaceful world is
     a great place for new players to get started in Minecraft, or for seasoned players to relax and enjoy
     the more creative aspects of the game.`,
@@ -21,33 +21,33 @@ export default {
   connectInstructions: {
     'Hub': [
       {
-        instruction: 'Connect to our network hub server with the following ip.',
+        instruction: 'Connect to our network hub server with the following ip:',
         code: 'peacefulcraft.net',
       },
       {
-        instruction: 'Use the following command to join the PvE Server.',
+        instruction: 'Use the following command to join the PvE Server:',
         code: '/server pve',
       },
       {
-        instruction: 'Use the following command to warp to the peaceful world',
+        instruction: 'Use the following command to warp to the peaceful world:',
         code: '/warp peaceful',
       },
       {
-        instruction: 'We recommend using the RTP command to get out of spawn and begin your adventure',
+        instruction: 'We recommend using the RTP command to get out of spawn and begin your adventure:',
         code: '/rtp'
       },
     ],
     'Direct Connect': [
       {
-        instruction: 'Direct connect to PvE with the follow ip.',
+        instruction: 'Direct connect to PvE with the follow ip:',
         code: 'pve.peacefulcraft.net',
       },
       {
-        instruction: 'Use the following command to warp to the peaceful world',
+        instruction: 'Use the following command to warp to the peaceful world:',
         code: '/warp peaceful',
       },
       {
-        instruction: 'We recommend using the RTP command to get out of spawn and begin your adventure',
+        instruction: 'We recommend using the RTP command to get out of spawn and begin your adventure:',
         code: '/rtp'
       },
     ],
