28960f3c35aded8f20090577e5dd878ff10cd662
Jumbie247 <65747242+Jumbie247@users.noreply.github.com>
2020-12-31 20:39:22 +0000

    some proof changes

--- src/copy/gamemodes/Trench.js
+++ src/copy/gamemodes/Trench.js
@@ -10,8 +10,8 @@ export default {
   },
   
   infoBlurb: `TrenchPvP is a custom PvP game mode developed in house by the PeacefulCraft Network. Trench is a KitPvP
-    inspired TMD game mode that has heavy influences from Vavle's TeamFortress 2. Right now, that means players are
-    split into two teams and have ten minutes go get as many kills for their team as they can. All the kits have custom
+    inspired TDM game mode that has heavy influences from Vavle's TeamFortress 2. Right now, that means players are
+    split into two teams and have ten minutes get as many kills for their team as they can. All the kits have custom
     weapons and or abilities; anything from invisibility, kill combo buffs, double jump, to grenade launchers. All
     classes are designed to have unique play-styles and excel situationally. The dev team has plans to expand Trench
     to include more objective-oriented game modes like capture the flag and area control.`,
