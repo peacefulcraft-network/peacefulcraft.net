342073145f3722568ca16f1e266cb713228977c8
Parsonswy <parsonswy@gmail.com>
2023-03-01 23:31:01 -0500

    bump deps

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,14 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "bin"),
-    filename: "app.[hash:8].js",
-    chunkFilename: "[name].bit.[hash:8].js",
+    filename: "app.[chunkhash].js",
+    chunkFilename: "[name].bit.[chunkhash].js",
     publicPath: "/"
   },
   devtool: "source-map",
   target: "web",
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    static: path.join(__dirname, 'public'),
     compress: true,
     hot: true
   },
